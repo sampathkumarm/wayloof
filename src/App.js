cea94d22cbc3044e42d283c47c9add5c50558ab0
Sampath Kumar <sgm@Martech.local>
2020-03-22 23:48:42 -0700

    Updated default React assets to Wayloof.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.scss";
+import { Typography } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="h1" color="primary">
+          Welcome to Wayloof...
+        </Typography>
       </header>
     </div>
   );
